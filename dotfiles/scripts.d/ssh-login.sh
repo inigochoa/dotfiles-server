b3dd6381d58e0ffb39f6bab8b1d9d9f756a19e28
Iñigo Ochoa <inigochoa@gmail.com>
2025-10-26 09:57:13 +0100

    fix(scripts.d): remove unnecessary ip

--- dotfiles/scripts.d/ssh-login.sh
+++ dotfiles/scripts.d/ssh-login.sh
@@ -2,13 +2,11 @@
 
 if [ "$PAM_TYPE" != "close_session" ]; then
   HOSTNAME=$(hostname)
-  IP=$(echo $SSH_CONNECTION | awk '{print $1}')
 
   MESSAGE="🔐 <b>New SSH Login</b>
 ━━━━━━━━━━━━━━━━
 👤 <b>User:</b> ${PAM_USER}
 🖥  <b>Server:</b> ${HOSTNAME}
-🌍 <b>Source IP:</b> ${IP}
 ⏰ $(date '+%Y-%m-%d %H:%M:%S')"
 
   ~/.scripts.d/telegram-notify.sh "${MESSAGE}"
